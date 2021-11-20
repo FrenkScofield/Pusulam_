f99be1f6570e8a6aa32789494cdaad301c6d054c
Sefa Çaksu <sefacaksu@techlife.com.tr>
2021-11-20 13:01:09 +0300

    Tkt aktarım yapılıyor

---
+++ Pusulam/Controllers/Tkt/TKTAktarimController.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using PusulamBusiness;
+using PusulamBusiness.Tkt;
+using PusulamBusiness.Enums;
+using System;
+using System.Web.Http;
+
+namespace Pusulam.Controllers.Tkt
+{
+    public class TKTAktarimController : ApiController
+    {
+        internal int ID_MENU = (int)EMenu.TktAktarim;
+
+        public Object TktAktar(JObject j)
+        {
+            try
+            {
+                using (Channel2<DTKTAktarim> c = new Channel2<DTKTAktarim>(ID_MENU))
+                {
+                    return c._cs.TktAktar(j);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
